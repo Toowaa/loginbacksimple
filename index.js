b4518fb5c6079e80bfeb2f1a3883ccce53bd610d
Toowaa <brahanax@gmail.com>
2025-07-05 12:49:18 -0500

    add to vercel

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const app = express();
-const PORT = 3000;
-
-app.use(cors());
-app.use(bodyParser.json());
-
 const validUsers = [
   { email: "brahan@fake.com", password: "1234567859" },
   { email: "ana@test.com", password: "pass1234" },
@@ -16,20 +6,20 @@ const validUsers = [
   { email: "sofia@fake.com", password: "abcd1234" },
 ];
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
 
-  const userFound = validUsers.find(
-    (user) => user.email === email && user.password === password
-  );
+    const userFound = validUsers.find(
+      (user) => user.email === email && user.password === password
+    );
 
-  if (userFound) {
-    res.status(200).json({ success: true, message: "Login exitoso" });
+    if (userFound) {
+      res.status(200).json({ success: true, message: "Login exitoso" });
+    } else {
+      res.status(401).json({ success: false, message: "Credenciales inválidas" });
+    }
   } else {
-    res.status(401).json({ success: false, message: "Credenciales inválidas" });
+    res.status(405).json({ message: "Método no permitido" });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+}
